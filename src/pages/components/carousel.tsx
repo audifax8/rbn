440c830f2734358d21a3c54f6586966299d0211d
Javier Gómez <franciscog@thisisyr.com>
2025-02-13 18:10:00 -0500

    sanitiza configure

--- src/pages/components/carousel.tsx
+++ src/pages/components/carousel.tsx
@@ -8,6 +8,7 @@ export default function Carousel(props: any) {
     <></>
   );*/
   const { configure } = props;
+  if (!configure) { return; }
   const options = {
     type: 'displayCarousel',
     container: '.fc-carousel-wrapper',
